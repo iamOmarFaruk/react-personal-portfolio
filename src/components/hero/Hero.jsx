7e4acf1ca9752f10f52362fd9a5e1064068f487f
Omar Faruk <omarbg.bd@gmail.com>
2024-01-22 14:44:20 +0600

    Refactor Hero component styles

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
 import { motion } from 'framer-motion';
-import './hero.scss';
+import styles from './Hero.module.scss'; // import styles
 
 const Hero = () => {
   const [isInView, setIsInView] = useState(false);
@@ -43,46 +43,57 @@ const Hero = () => {
   };
   
   return (
-    <div className="hero">
-      <div className="wrapper">
-        <div className="textContainer">
+    <div className={styles.hero}>
+      <div className={styles.wrapper}>
+        <div className={styles.textContainer}>
           <h2>Omar Faruk</h2>
-          <h1>A Fullstack
+          <h1>
+            A Fullstack
             <br />
-            Web Developer</h1>
-          <div className="buttons">
-            <button className="btn">Contact Me</button>
-            <button className="btn-secondary">Download CV</button>
+            Web Developer
+          </h1>
+          <p className={styles['hero-content']}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing <br /> metus sed
+            ligula interdum ullamcorper varius
+          </p>
+          <div className={styles.buttons}>
+            <button className={styles.btn}>Contact Me</button>
+            <button className={styles['btn-secondary']}>Download CV</button>
           </div>
           <motion.img
             ref={scrollRef}
-            className='indicator'
+            className={styles.indicator}
             src="/scroll.png"
             alt="Scroll Indicator"
             variants={scrollIndicatorVariants}
             initial="up"
             animate={isInView ? { y: ["-10px", "10px"] } : {}}
-            transition={isInView ? {
-              repeatType: "mirror",
-              repeat: Infinity,
-              duration: 1,
-              ease: "easeInOut"
-            } : {}}
+            transition={
+              isInView
+                ? {
+                    repeatType: "mirror",
+                    repeat: Infinity,
+                    duration: 1,
+                    ease: "easeInOut",
+                  }
+                : {}
+            }
           />
         </div>
 
         {isInView && (
           <motion.div
-            className='slidingTextContainer'
+            className={styles.slidingTextContainer}
             variants={slidingTextVariants}
             initial="animate"
             animate="animate"
           >
-            REACT, NODE, MONGODB, MYSQL, SASS, JAVASCRIPT, TYPESCRIPT, GIT, GITHUB, HEROKU, NETLIFY
+            REACT, NODE, MONGODB, MYSQL, SASS, JAVASCRIPT, TYPESCRIPT, GIT,
+            GITHUB, HEROKU, NETLIFY
           </motion.div>
         )}
 
-        <div className="imageContainer">
+        <div className={styles.imageContainer}>
           <img src="/hero.png" alt="Hero" />
         </div>
       </div>
