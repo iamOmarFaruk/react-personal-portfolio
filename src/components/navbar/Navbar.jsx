6f7ac39cb8ee5676e273c3d1a0078f4481a8beda
Omar Faruk <omarbg.bd@gmail.com>
2024-01-22 15:13:49 +0600

    Update Hero and Navbar styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,18 @@
-import "./navbar.scss";
+import styles from "./navbar.module.scss";
 const Navbar = () => {
   return (
     <>
-      <div className="navbar">
-       
-        <div className="wrapper">
-          <span>
+      <div className={styles.navbar}>
+        <div className={styles.wrapper}>
+          <a href="/" className={styles.logo}>
             React Folio
-          </span>
-
-
-
-
-          <div className="nav-menu">
-            <a className="nav-item">Home</a>
-            <a className="nav-item">About</a>
-            <a className="nav-item">Services</a>
-            <a className="nav-item">Portfolio</a>
-            <a className="nav-item">Contact</a>
+          </a>
+          <div className={styles["nav-menu"]}>
+            <a className={styles["nav-item"]}>Home</a>
+            <a className={styles["nav-item"]}>About</a>
+            <a className={styles["nav-item"]}>Services</a>
+            <a className={styles["nav-item"]}>Portfolio</a>
+            <a className={styles["nav-item"]}>Contact</a>
           </div>
         </div>
       </div>
