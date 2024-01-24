364ca3e2e145061dde3fc6c6078412d4d9ba0807
Omar Faruk <omarbg.bd@gmail.com>
2024-01-25 00:00:04 +0600

    Add react-type-animation package and implement TypeAnimation component in Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { TypeAnimation } from "react-type-animation";
 import styles from "./navbar.module.scss";
 const Navbar = () => {
   return (
@@ -5,7 +6,23 @@ const Navbar = () => {
       <div className={styles.navbar}>
         <div className={styles.wrapper}>
           <a href="/" className={styles.logo}>
-            <pre><code>{`<ReactDeveloper/>`}</code></pre>
+            <TypeAnimation
+              cursor={true}
+              sequence={[
+               
+                "<ReactDeveloper/>",
+                2000,
+                "<HireMeNow/>",
+                1000,
+                "<ForMakingAwesome/>",
+                1000,
+                "<ReactProject/>",
+                2000,
+              ]}
+              wrapper="pre"
+              repeat={Infinity}
+             
+            />
           </a>
           <div className={`${styles["nav-menu"]} nav-cta`}>
             <a className={styles["nav-item"]}>Home</a>
@@ -14,7 +31,9 @@ const Navbar = () => {
             <a className={styles["nav-item"]}>Portfolio</a>
             <a className={styles["nav-item"]}>Contact</a>
             {/* hire me - ctc button */}
-            <a className={`${styles["nav-item"]} ${styles["nav-cta-button"]}`}>Hire Me</a>
+            <a className={`${styles["nav-item"]} ${styles["nav-cta-button"]}`}>
+              Hire Me
+            </a>
           </div>
         </div>
       </div>
